bf96319a3b01cecc80b3c3770b8383fd89460d10
Md. Safwan <sayedmdsafwan@gmail.com>
2022-03-01 04:32:12 +0600

    condition applied

--- js/script.js
+++ js/script.js
@@ -1,44 +1,56 @@
 const loadMobileName = () => {
     const searchContent = document.getElementById("search-field");
     const searchText = searchContent.value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    fetch(url)
-        .then((Response) => Response.json())
-        .then((json) => displayMobileName(json.data));
     searchContent.value = "";
+    if (searchText == "") {
+        alert("Please give some food name");
+    } else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+        fetch(url)
+            .then((Response) => Response.json())
+            .then((json) => displayMobileName(json.data));
+    }
 };
 
 const displayMobileName = (mobiles) => {
     // console.log(mobiles);
     const displayMobiles = document.getElementById("display-search");
     displayMobiles.textContent = "";
-    mobiles.forEach((mobile) => {
-        console.log(mobile);
-        const div = document.createElement("div");
-        div.classList.add("col-md-6");
-        div.innerHTML = `
-            <div class="card mb-3" >
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img
-                            src="${mobile.image}"
-                            class="img-fluid rounded-start"
-                        />
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h3 class="card-title">${mobile.phone_name}</h3>
-                            <p class="card-text">
-                                Brand: ${mobile.brand}
-                            </p>
-                            <button class="btn btn-outline-primary" onclick="loadMobileId('${mobile.slug}')">Details</button>
+    if (mobiles.length == 0) {
+        const hiddenDiv = document.createElement("div");
+        hiddenDiv.innerHTML = `
+            <h3 class="text-center"> Please give a correct phone name</h3> 
+        `;
+        displayMobiles.appendChild(hiddenDiv);
+    } else {
+        mobiles.forEach((mobile) => {
+            console.log(mobile);
+            const div = document.createElement("div");
+            div.classList.add("col-md-6");
+            div.innerHTML = `
+                <div class="card mb-3" >
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img
+                                src="${mobile.image}"
+                                class="img-fluid rounded-start"
+                            />
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h3 class="card-title">${mobile.phone_name}</h3>
+                                <p class="card-text">
+                                    Brand: ${mobile.brand}
+                                </p>
+                                <button class="btn btn-outline-primary" onclick="loadMobileId('${mobile.slug}')">Details</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        `;
-        displayMobiles.appendChild(div);
-    });
+            `;
+            displayMobiles.appendChild(div);
+        });
+    }
 };
 
 const loadMobileId = (mobileId) => {
@@ -72,7 +84,9 @@ const displayMobileData = (mobileData) => {
                 <li>${mobileData.mainFeatures.chipSet}</li>
                 <li>${mobileData.mainFeatures.displaySize}</li>
             </ul>
-            <p class="lead"><b>Sensors:</b> ${mobileData.mainFeatures.sensors.join(', ')}</p>
+            <p class="lead"><b>Sensors:</b> ${mobileData.mainFeatures.sensors.join(
+                ", "
+            )}</p>
         </div>
     `;
     detailsData.appendChild(div);
